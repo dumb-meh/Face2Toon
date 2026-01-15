d84af139aea672a3b27d5a873202db656c8614ea
dumb-meh <omorfaruk549@gmail.com>
2026-01-15 12:00:42 +0600

    minor changes

--- app/services/Create_Character/create_character_schema.py
+++ app/services/Create_Character/create_character_schema.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel
 from typing import Optional, List
 
 class CreateCharacterRequest(BaseModel):
-    past_characters: Optional[List[str]] = None
+    past_characters: Optional[List[List]] = None
     past_themes: Optional[List[str]] = None
 
 class CreateCharacterResponse(BaseModel):
